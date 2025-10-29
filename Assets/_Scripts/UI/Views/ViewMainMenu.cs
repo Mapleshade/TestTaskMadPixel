bd7d238541f41ca20c7dd3374ee1944d3d8650ca
Elina Gazizova <e89600342003@gmail.com>
2025-10-29 16:27:57 +0300

    add text mesh pro

--- Assets/_Scripts/UI/Views/ViewMainMenu.cs
+++ Assets/_Scripts/UI/Views/ViewMainMenu.cs
@@ -7,15 +7,6 @@ public class ViewMainMenu : BaseUIView
 {
 	[SerializeField]
 	private Button _newGameButton;
-	[SerializeField]
-	private Button _loadGameButton;
-	[SerializeField]
-	private Button _settingsButton;
-	[SerializeField]
-	private Button _exitButton;
 
 	public Button NewGameButton => _newGameButton;
-	public Button LoadGameButton => _loadGameButton;
-	public Button SettingsButton => _settingsButton;
-	public Button ExitButton => _exitButton;
 }
