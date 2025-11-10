a22a6327f54f796f2f457fd889182b10d166d3d7
Elina Gazizova <e89600342003@gmail.com>
2025-11-10 19:53:09 +0300

    start move logic from game presenter

---
+++ Assets/_Scripts/Models/GameController.cs
@@ -0,0 +1,13 @@
+﻿using Zenject;
+
+public class GameController
+{
+	private readonly GamePresenter _gamePresenter;
+	private readonly SignalBus _signalBus;
+
+	public GameController(GamePresenter gamePresenter, SignalBus signalBus)
+	{
+		_gamePresenter = gamePresenter;
+		_signalBus = signalBus;
+	}
+}
